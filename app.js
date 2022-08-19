300917c01419020ada16fc62e370a3d29194a75d
BuntaEU <93155128+BuntaEU@users.noreply.github.com>
2022-08-19 22:47:16 +0200

    Added Grid

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ const resetButton = document.getElementById("resetButton");
 const sizeValue = document.getElementById("sizeValue");
 const sizeSlider = document.getElementById("sizeSlider");
 const grid = document.getElementById("grid");
+const toggle = document.getElementById("toggleGrid");
+
 
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
 colorButton.onclick = () => setCurrentMode("color");
@@ -34,10 +36,12 @@ eraserButton.onclick = () => setCurrentMode("eraser");
 resetButton.onclick = () => reloadGrid();
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
+toggle.onclick =() => toggleGrid();
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
+
 
 function changeSize(value) {
 	setCurrentSize(value);
@@ -60,11 +64,16 @@ function setupGrid(size) {
 	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 	for (let i = 0; i < size * size; i++) {
 		const gridElement = document.createElement("div");
-		gridElement.classList.add("grid-element");
+		gridElement.classList.add("grid-element") ;
+		gridElement.classList.add("border-grid");
 		gridElement.addEventListener("mouseover", changeColor);
 		gridElement.addEventListener("mousedown", changeColor);
 		grid.appendChild(gridElement);
 	}
+}
+
+function toggleGrid(){
+	grid.classList.toggle("deactivated");
 }
 function changeColor(e) {
 	if (e.type == "mouseover" && !mouseDown) return;
